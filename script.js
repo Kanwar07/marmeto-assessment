de2750b218cd9850ad0927ccbd92500839de8aa7
Kanwar07 <anmolkanwar07@gmail.com>
2024-02-14 02:23:06 +0530

    Major functionality

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+async function getdata() {
+  try {
+    const response = await fetch(
+      "https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export { getdata };
